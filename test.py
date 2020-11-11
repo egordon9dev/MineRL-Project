62f475ab59bf5bdddafa5e68ddeeb7a7132a848a
Max Rudolph <maxrudolph@Maxs-MacBook-Pro-3.local>
2020-11-11 10:50:30 -0500

    added DQN stuff

--- test.py
+++ test.py
@@ -10,14 +10,14 @@ net_reward = 0
 while not done:
     action = env.action_space.noop()
 
-    action['camera'] = [0, 0.03*obs["compassAngle"]]
+    action['camera'] = [0, 0.03]
     action['back'] = 0
     action['forward'] = 1
     action['jump'] = 1
     action['attack'] = 1
 
     obs, reward, done, info = env.step(
-        action)
+        env.action_space.sample())
 
     net_reward += reward
     print("Total reward: ", net_reward)
